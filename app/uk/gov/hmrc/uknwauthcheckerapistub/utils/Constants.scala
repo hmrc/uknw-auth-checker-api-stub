80589fca4d3ede0cdb9211a35a4a347a15a0b656
GaneshMaddipoti <16971000+GaneshMaddipoti@users.noreply.github.com>
2024-08-02 09:29:18 +0100

    NP-253: Refactored code

--- app/uk/gov/hmrc/uknwauthcheckerapistub/utils/Constants.scala
+++ app/uk/gov/hmrc/uknwauthcheckerapistub/utils/Constants.scala
@@ -1,6 +1,6 @@
 package uk.gov.hmrc.uknwauthcheckerapistub.utils
 
 object Constants {
-  val path = "/cau/validatecustomsauth/v1"
+  val path        = "/cau/validatecustomsauth/v1"
   val bearerToken = "Bearer PFZBTElEX1RPS0VOPg=="
 }
